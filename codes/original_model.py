255c0cd476716a03d02eed9e5278028b12923439
Buz Galbraith <buzgalbraith.gmail.com>
2023-10-31 14:45:09 -0400

    made changes to negative sampeling method

--- codes/original_model.py
+++ codes/original_model.py
@@ -315,14 +315,14 @@ class KGEModel(nn.Module):
         '''
         Evaluate the model on test or valid datasets
         '''
-
+        
         model.eval()
-
+        
         if args.countries:
-            # Countries S* datasets are evaluated on AUC-PR
-            # Process test data for AUC-PR evaluation
+            #Countries S* datasets are evaluated on AUC-PR
+            #Process test data for AUC-PR evaluation
             sample = list()
-            y_true = list()
+            y_true  = list()
             for head, relation, tail in test_triples:
                 for candidate_region in args.regions:
                     y_true.append(1 if candidate_region == tail else 0)
@@ -337,112 +337,92 @@ class KGEModel(nn.Module):
 
             y_true = np.array(y_true)
 
-            # average_precision_score is the same as auc_pr
+            #average_precision_score is the same as auc_pr
             auc_pr = average_precision_score(y_true, y_score)
 
             metrics = {'auc_pr': auc_pr}
-
+            
         else:
-            # Otherwise use standard (filtered) MRR, MR, HITS@1, HITS@3, and HITS@10 metrics
-            # Prepare dataloader for evaluation
+            #Otherwise use standard (filtered) MRR, MR, HITS@1, HITS@3, and HITS@10 metrics
+            #Prepare dataloader for evaluation
+            test_dataloader_head = DataLoader(
+                TestDataset(
+                    test_triples, 
+                    all_true_triples, 
+                    args.nentity, 
+                    args.nrelation, 
+                    'head-batch'
+                ), 
+                batch_size=args.test_batch_size,
+                num_workers=max(1, args.cpu_num//2), 
+                collate_fn=TestDataset.collate_fn
+            )
 
-            # Organize your test triples by relation
-            triplets_by_relation = {}
-            for test_triple in test_triples:
-                head, relation, tail = test_triple
-                relation = relation.item()
-                if relation not in triplets_by_relation:
-                    triplets_by_relation[relation] = []
-                triplets_by_relation[relation].append(test_triple)
-
+            test_dataloader_tail = DataLoader(
+                TestDataset(
+                    test_triples, 
+                    all_true_triples, 
+                    args.nentity, 
+                    args.nrelation, 
+                    'tail-batch'
+                ), 
+                batch_size=args.test_batch_size,
+                num_workers=max(1, args.cpu_num//2), 
+                collate_fn=TestDataset.collate_fn
+            )
+            
+            test_dataset_list = [test_dataloader_head, test_dataloader_tail]
+            
             logs = []
 
             step = 0
-            total_steps = len(test_triples)
+            total_steps = sum([len(dataset) for dataset in test_dataset_list])
 
             with torch.no_grad():
-                for relation, test_relation_triples in triplets_by_relation.items():
-                    test_dataloader_head = DataLoader(
-                        TestDataset(
-                            test_relation_triples,
-                            all_true_triples,
-                            args.nentity,
-                            args.nrelation,
-                            'head-batch'
-                        ),
-                        batch_size=args.test_batch_size,
-                        num_workers=max(1, args.cpu_num // 2),
-                        collate_fn=TestDataset.collate_fn
-                    )
+                for test_dataset in test_dataset_list:
+                    for positive_sample, negative_sample, filter_bias, mode in test_dataset:
+                        if args.cuda:
+                            positive_sample = positive_sample.cuda()
+                            negative_sample = negative_sample.cuda()
+                            filter_bias = filter_bias.cuda()
 
-                    test_dataloader_tail = DataLoader(
-                        TestDataset(
-                            test_relation_triples,
-                            all_true_triples,
-                            args.nentity,
-                            args.nrelation,
-                            'tail-batch'
-                        ),
-                        batch_size=args.test_batch_size,
-                        num_workers=max(1, args.cpu_num // 2),
-                        collate_fn=TestDataset.collate_fn
-                    )
+                        batch_size = positive_sample.size(0)
 
-                    for test_dataset in [test_dataloader_head, test_dataloader_tail]:
-                        for positive_sample, negative_sample, filter_bias, mode in test_dataset:
-                            if args.cuda:
-                                positive_sample = positive_sample.cuda()
-                                negative_sample = negative_sample.cuda()
-                                filter_bias = filter_bias.cuda()
+                        score = model((positive_sample, negative_sample), mode)
+                        score += filter_bias
 
-                            batch_size = positive_sample.size(0)
+                        #Explicitly sort all the entities to ensure that there is no test exposure bias
+                        argsort = torch.argsort(score, dim = 1, descending=True)
 
-                            # Ensure that negative samples are of the same relation
-                            relation = positive_sample[0][1].item()
-                            negative_samples = torch.stack(triplets_by_relation[relation])
-                            negative_samples = negative_samples[negative_samples != positive_sample]
+                        if mode == 'head-batch':
+                            positive_arg = positive_sample[:, 0]
+                        elif mode == 'tail-batch':
+                            positive_arg = positive_sample[:, 2]
+                        else:
+                            raise ValueError('mode %s not supported' % mode)
 
-                            if len(negative_samples) < batch_size:
-                                # Handle the case where there are not enough negative samples of the same relation
-                                continue
+                        for i in range(batch_size):
+                            #Notice that argsort is not ranking
+                            ranking = (argsort[i, :] == positive_arg[i]).nonzero()
+                            assert ranking.size(0) == 1
 
-                            negative_samples = negative_samples[:batch_size]
+                            #ranking + 1 is the true ranking used in evaluation metrics
+                            ranking = 1 + ranking.item()
+                            logs.append({
+                                'MRR': 1.0/ranking,
+                                'MR': float(ranking),
+                                'HITS@1': 1.0 if ranking <= 1 else 0.0,
+                                'HITS@3': 1.0 if ranking <= 3 else 0.0,
+                                'HITS@10': 1.0 if ranking <= 10 else 0.0,
+                            })
 
-                            score = model((positive_sample, negative_samples), mode)
-                            score += filter_bias
+                        if step % args.test_log_steps == 0:
+                            logging.info('Evaluating the model... (%d/%d)' % (step, total_steps))
 
-                            # Explicitly sort all the entities to ensure that there is no test exposure bias
-                            argsort = torch.argsort(score, dim=1, descending=True)
-
-                            if mode == 'head-batch':
-                                positive_arg = positive_sample[:, 0]
-                            elif mode == 'tail-batch':
-                                positive_arg = positive_sample[:, 2]
-                            else:
-                                raise ValueError('mode %s not supported' % mode)
-
-                            for i in range(batch_size):
-                                # Notice that argsort is not ranking
-                                ranking = (argsort[i, :] == positive_arg[i]).nonzero()
-                                assert ranking.size(0) == 1
-
-                                # ranking + 1 is the true ranking used in evaluation metrics
-                                ranking = 1 + ranking.item()
-                                logs.append({
-                                    'MRR': 1.0 / ranking,
-                                    'MR': float(ranking),
-                                    'HITS@1': 1.0 if ranking <= 1 else 0.0,
-                                    'HITS@3': 1.0 if ranking <= 3 else 0.0,
-                                    'HITS@10': 1.0 if ranking <= 10 else 0.0,
-                                })
-
-                            if step % args.test_log_steps == 0:
-                                logging.info('Evaluating the model... (%d/%d)' % (step, total_steps))
-
-                            step += 1
+                        step += 1
 
             metrics = {}
             for metric in logs[0].keys():
-                metrics[metric] = sum([log[metric] for log in logs]) / len(logs)
+                metrics[metric] = sum([log[metric] for log in logs])/len(logs)
 
         return metrics
